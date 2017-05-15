539a4a5b292e597ebbf132721b48489524abb741
Gunpreet Singh <gunpreet@outlook.com>
2017-05-15 12:17:12 -0700

    empty array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+  // refer to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof 
+    // for type of's'
+
+  // edge cases 
+    // type of object is a function or undefined 
+      // return undefined 
+
+  if (typeof obj === 'function ' || typeof obj === 'undefined') {
+    return undefined;
+    // if type of obejct is number or boolean 
+      // convert object to a strin g
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+    // if the type of object is object but value is null 
+      // convert null to string -- can hard code or use '"' + null + '"'
+  } else if (typeof obj === 'object' && obj === null) {
+    return 'null';
+    //if  type of is string , add addtional quatoations before and after object 
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  // if is an array 
+  } else if (typeof obj === 'object' && Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      
+    }
+  }
 };
